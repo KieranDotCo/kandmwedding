d805cb7bf208fb52185ae08c714c53b7dcd7eb89
Kieran Whiteman <kieran@kieran.co>
2023-01-04 15:45:33 +0000

    Update page not found

--- src/routes/PageNotFound/PageNotFound.tsx
+++ src/routes/PageNotFound/PageNotFound.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import bgTop from "../../assets/images/small-bg-top.png";
+import bgBottom from "../../assets/images/small-bg-bottom.png";
 
 const PageNotFound: React.FC = () => {
   return (
-    <div className="bg-grey caligraphy-text" style={{  padding: '5rem 0' }}>
+    <div className="bg-grey caligraphy-text ta-center" style={{  padding: '5rem 0' }}>
+      <img src={bgTop} alt="bg top" className="bg-top" />
       <p className="ta-center" style={{ margin: "0 auto", fontSize: "100px"}}>
         Page Not Found
       </p>
+      <img src={bgBottom} alt="bg bottom" className="bg-bottom" />
     </div>
   );
 };
